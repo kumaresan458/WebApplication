111635bc6f1019e15570ef765b9c7e075eb84b31
kumaresan.m <kumaresan.m@kumaresan.tagitmobile.com>
2019-08-21 10:43:48 +0530

    db issues resolving

--- src/main/java/com/employee/dao/impl/EmployeeDaoImpl.java
+++ src/main/java/com/employee/dao/impl/EmployeeDaoImpl.java
@@ -1,9 +1,16 @@
 package com.employee.dao.impl;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.employee.bean.Employee;
 import com.employee.dao.EmployeeDao;
 
 public class EmployeeDaoImpl implements EmployeeDao {
+
+	@Autowired
+	private DataSource dataSource;
 
 	public Employee login(String userName, String password) {
 
